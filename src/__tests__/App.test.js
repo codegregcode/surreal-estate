8e5265aa83a50fa3296ccac9fa90bb6886a89a92
Greg Cain <codegregcode@gmail.com>
2023-04-17 21:42:01 +0100

    updates App component, rewrites App test

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,8 +2,10 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "../components/App";
 
-test("renders surreal estate text", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Surreal Estate/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  const { asFragment } = render(<App />);
+
+  it("render App component", () => {
+    expect(asFragment()).toMatchSnapshot();
+  });
 });
