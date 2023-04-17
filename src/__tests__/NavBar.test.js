224e10921bfdd7c0b64d19154a2aa36084301e12
Greg Cain <codegregcode@gmail.com>
2023-04-17 21:26:09 +0100

    writes & passes NavBar tests

---
+++ src/__tests__/NavBar.test.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import NavBar from "../components/NavBar";
+
+describe("NavBar", () => {
+  const { asFragment } = render(<NavBar />);
+
+  it("renders navbar component", () => {
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("contains list/link items", () => {
+    render(<NavBar />);
+    const viewProperties = screen.getByText(/View Properties/);
+    const addAProperty = screen.getByText(/Add a Property/);
+
+    expect(viewProperties).toBeInTheDocument();
+    expect(addAProperty).toBeInTheDocument();
+  });
+});
